6781b94297e52160912485a57d0872f45b27567e
STEVEN A BOWMAN <stevenabowman@STEVENs-MacBook-Pro.local>
2024-12-10 15:23:52 -0500

    Server Hello from Dall-E

--- server/index.js
+++ server/index.js
@@ -4,16 +4,16 @@ import cors from 'cors';
 
 dotenv.config();
 
-const app= express();
+const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
 app.get('/', async (req, res) => {
-    res.send('Hello from DALL-E!');
+  res.send('Hello from DALL-E!');
 })
 
 const startServer = async () => {
-    app.listen(8080, () => console.log('Server has started on port http://localhost:8080'))
+ app.listen(8080, () => console.log('Server has started on port http://localhost:8080'))
 }
 
 startServer();
